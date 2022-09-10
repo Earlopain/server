0cd93e610164b8fab780e4797663fb9cace86be4
Earlopain <earlopain@protonmail.com>
2022-09-10 23:14:32 +0200

    Add jellyfin

--- startup.sh
+++ startup.sh
@@ -4,3 +4,4 @@ sudo cryptsetup luksOpen /dev/disk/by-uuid/026e8027-ce43-4520-8eda-7b193d26c8cd 
 sudo mount /dev/mapper/ssd-crypt /mnt/ssd
 
 docker-compose -f $SERVER_PROJECT_DIR/code-server/docker-compose.yml up
+docker-compose -f $SERVER_PROJECT_DIR/jellyfin/docker-compose.yml up
