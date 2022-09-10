ebc7680742e29fc55fd1587f05ed05dc8508cae3
Earlopain <earlopain@protonmail.com>
2022-09-10 23:55:32 +0200

    Add samba

--- startup.sh
+++ startup.sh
@@ -3,5 +3,6 @@
 sudo cryptsetup luksOpen /dev/disk/by-uuid/026e8027-ce43-4520-8eda-7b193d26c8cd ssd-crypt
 sudo mount /dev/mapper/ssd-crypt /mnt/ssd
 
-docker-compose -f $SERVER_PROJECT_DIR/code-server/docker-compose.yml up
-docker-compose -f $SERVER_PROJECT_DIR/jellyfin/docker-compose.yml up
+docker-compose -f $SERVER_PROJECT_DIR/code-server/docker-compose.yml up -d
+docker-compose -f $SERVER_PROJECT_DIR/jellyfin/docker-compose.yml up -d
+docker-compose -f $SERVER_PROJECT_DIR/samba/docker-compose.yml up -d
