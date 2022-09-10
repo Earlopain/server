99551a292d9cbc95ba276b3d40052f9e3e1b9ddc
Earlopain <earlopain@protonmail.com>
2022-09-10 21:11:20 +0200

    Don't autostart code server

--- startup.sh
+++ startup.sh
@@ -2,3 +2,5 @@
 
 sudo cryptsetup luksOpen /dev/disk/by-uuid/026e8027-ce43-4520-8eda-7b193d26c8cd ssd-crypt
 sudo mount /dev/mapper/ssd-crypt /mnt/ssd
+
+docker-compose -f $SERVER_PROJECT_DIR/code-server/docker-compose.yml up
